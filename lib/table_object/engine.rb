51efe85d1d239f87e76a62f0b88749ab4d61030c
TomBosmans <t.bosse@hotmail.com>
2018-05-06 16:46:50 +0200

    moved required to bottom in engine.rb

--- lib/table_object/engine.rb
+++ lib/table_object/engine.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'table_object/base'
-
 module TableObject
   class Engine < ::Rails::Engine
     isolate_namespace TableObject
   end
 end
+
+require 'table_object/base'
